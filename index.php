dd681a448d95ff2da504a406cc9924144e6ff840
zirobelle <30334708+zirobelle@users.noreply.github.com>
2017-08-16 21:22:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$id],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
