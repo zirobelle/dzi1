ca77cc41d776783c2f4499f69f18e5602ae63711
zirobelle <30334708+zirobelle@users.noreply.github.com>
2017-08-16 20:44:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,13 +25,28 @@ if (!is_null($events['events'])) {
 
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/reply';
-{
-   "type": "text",
-   "text": "Hello, world"
-}
+      $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages],
+      ];
 
 
-
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+-d '{
+    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
+}' https://api.line.me/v2/bot/message/reply
 
 
       
